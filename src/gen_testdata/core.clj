705bb24a90c9e19d3d5e69041bfd17e89ca42f73
Yurii <tovy@1c.ru>
2020-09-16 21:28:41 +0300

    Add list of imports

--- src/gen_testdata/core.clj
+++ src/gen_testdata/core.clj
@@ -1,1 +1,6 @@
-(ns gen-testdata.core)
+(ns gen-testdata.core
+  (:require gen-testdata.user-import
+            gen-testdata.xml
+            gen-testdata.reg-jsons
+            gen-testdata.shitload)
+  (:gen-class))
